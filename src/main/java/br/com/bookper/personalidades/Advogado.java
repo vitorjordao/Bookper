0856d48aef49f01a5ef735a8b024243795f3f9fd
Vitor Jordão <srvitorlol@gmail.com>
2017-12-10 18:20:47 -0200

    Subindo Arquivos para testes
    Arquivos para testes com o maven

---
+++ src/main/java/br/com/bookper/personalidades/Advogado.java
@@ -0,0 +1,18 @@
+package br.com.bookper.personalidades;
+
+public class Advogado implements Personalidades{
+	private Personalidades outraPersonalidade;
+	public void setProximo(Personalidades outraPersonalidade) {
+		this.outraPersonalidade = outraPersonalidade;
+	}
+	@Override
+	public Personalidades verificaPersonalidade(String personalidade) {
+		if(!personalidade.equals("Advogado")) {
+			return outraPersonalidade.verificaPersonalidade(personalidade);
+        }else {
+        	return new Advogado();
+        }
+		
+	}
+	
+}
