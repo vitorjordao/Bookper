834f07d576b46d7b13641f72080583605b4ec8bd
Vitor Jordao <srvitorlol@gmail.com>
2018-02-25 20:55:20 -0300

    Correção de icone e correção da validação dos nomes

--- src/main/java/br/com/bookper/validacoes/Nome.java
+++ src/main/java/br/com/bookper/validacoes/Nome.java
@@ -5,7 +5,7 @@ public class Nome implements Validacoes {
 	@Override
 	public TiposValidacoes isValido(final Object nome) {
 		final String nomeOk = (String) nome;
-		if (nomeOk.matches("[A-Z][a-z]{1,} [A-Z][a-z]{0,} [A-Z][a-z]{0,} [A-Z][a-z]{0,}") && nomeOk.length() <= 100) {
+		if (nomeOk.matches("^[a-zA-Z\\s]+") && nomeOk.length() <= 100) {
 			return TiposValidacoes.OK;
 
 		} else
